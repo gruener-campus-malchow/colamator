4e7b561286901f5d48a4fe4792f366c2f0824ad8
Noyavy <githubuser1@web.de>
2022-05-21 01:28:49 +0200

    added create queries

---
+++ doc/createTables.sql
@@ -0,0 +1,21 @@
+CREATE TABLE types (
+	'name' TEXT PRIMARY KEY,
+	label TEXT
+);
+
+CREATE TABLE gatherers (
+	username TEXT PRIMARY KEY,
+	last_seen INT DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE 'values' (
+	'name' TEXT PRIMARY KEY,
+	'comment' TEXT,
+	timestamp INTEGER DEFAULT CURRENT_TIMESTAMP,
+	content BLOB,
+	is_standard INTEGER,
+	typeID TEXT NOT NULL,
+	gathererID TEXT NOT NULL,
+	FOREIGN KEY (typeID) REFERENCES types(name),
+	FOREIGN KEY (gathererID) REFERENCES gatherers(username)
+);
