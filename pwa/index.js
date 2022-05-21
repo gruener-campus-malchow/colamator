1ed031651fdbc0620425d775e6f0b5aa2120ab64
Brian <maihoernchen.index@gmail.com>
2022-05-21 10:31:14 +0200

    duck

--- pwa/index.js
+++ pwa/index.js
@@ -1,8 +1,78 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-messaging.js";
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyBmSZlaJaPmZ0WASilJZEkIk6S6vtFl8Pw",
+
+    authDomain: "colamator.firebaseapp.com",
+
+    projectId: "colamator",
+
+    storageBucket: "colamator.appspot.com",
+
+    messagingSenderId: "395109243028",
+
+    appId: "1:395109243028:web:573f54b53f401a05b3fc81",
+
+    measurementId: "G-26FVCWP30J"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
+
+  const messaging = getMessaging(app);
+
+  getToken(messaging, {vapidKey: "BBaraOZkS0WlZSbIy4DnxepARsJ3D3bAkf3khUQ9e1eeonRJysSGmitGZxrmkle_PjCXKEsPF4fcFVF4ikgwILk"})
+  .then((currentToken) => {
+    if (currentToken) {
+      console.log("interact",currentToken);
+      const token = currentToken;
+    } else {
+      console.log("nops");
+    }
+  }).catch((err) => {
+    console.log("duck", err)
+  });
+
+
+const deviceType = () => {
+    const ua = navigator.userAgent;
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+        return "tablet";
+    }
+    else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+        return "mobile";
+    }
+    return "desktop";
+};
+
 fetch('/api/items/', {
   method: 'GET',
   //body: JSON.stringify({color: 'deine', type: 'mutter'})
 })
 .then(response => response.json())
 .then(json => {
-  document.getElementById('aha').innerText=json;
+  document.getElementById('aha').innerText=JSON.stringify(json);
+});
+
+window.addEventListener('appinstalled', (evt) => {
+  getToken(messaging, {vapidKey: "BBaraOZkS0WlZSbIy4DnxepARsJ3D3bAkf3khUQ9e1eeonRJysSGmitGZxrmkle_PjCXKEsPF4fcFVF4ikgwILk"})
+  .then((currentToken) => {
+    if (currentToken) {
+      console.log("interact",currentToken);
+      const token = currentToken;
+      fetch('/api/users/add_device_key', {
+        method: 'POST',
+        body: JSON.stringify({name:'Brian Cohen',device_key: token , device_name: deviceType})
+      })
+    } else {
+      console.log("nops");
+    }
+  }).catch((err) => {
+    console.log("duck", err)
+  });
 });
--- pwa/index.php
+++ pwa/index.php
@@ -8,17 +8,17 @@
 ?>
 <!DOCTYPE html>
 <head>
+	<link rel="manifest" href="manifest.json">
   <link rel="stylesheet" href="style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta charset="UTF-8" />
 	<meta name="theme-color" content="#1ac6ff">
 	<title >Trainings-Planer</title>
-	<link rel="manifest" href="manifest.json">
 	<link rel="apple-touch-icon" href="defeat.png">
-	<script language="javascript" type="text/javascript" src="index.js"></script>
+	<script language="javascript" type="module" src="index.js"></script>
  </head>
 <body>
-	<script type="text/javascript">
+	<script type="module">
 	if ('serviceWorker' in navigator) {
 	try {
 		navigator.serviceWorker.register('./sw.js');
