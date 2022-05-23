7f9dffdeb488b8819dd91ef715936b0304d54e1b
eintyp <eintyp@duck.com>
2022-05-23 15:41:03 +0200

    respond with 403

--- api/lib/ProtectedModel.php
+++ api/lib/ProtectedModel.php
@@ -5,7 +5,7 @@ class ProtectedModel extends Model
 
 	public function call($identifier, $attribute)
 	{
-		if (!Auth::getUser()) return;
+		if (!Auth::getUser()) return $this->api_response('Forbidden', 403);
 		parent::call($identifier, $attribute);
 	}
 
