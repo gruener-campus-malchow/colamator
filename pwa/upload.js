ef11f9301e0dccaef50401fe105388991406cb75
SkeL <maihoernchen.index@gmail.com>
2022-06-01 14:24:47 +0200

    duck the zucc

--- pwa/upload.js
+++ pwa/upload.js
@@ -1,16 +1,19 @@
-data = document.getElementById('data');
-var datatype;
-if (typeof data == number & data > 30 & data < 200) {
-  datatype = "Gewicht";
-} else if (typeof data == number & data > 100) {
-  datatype = "Kilokalorien";
-} else if (typeof data == number & data < 6) {
-  datatype = "Flüssigkeitszufuhr";
-} else {
-  datatype = "Kraft";
-}
+document.addEventListener("submit", uploady);
 
-fetch('/api/succ.php', {
-  method: 'POST',
-  body: JSON.stringify({datatype: data})
-}).then(response => response.text()).then(console.log);
+function uploady() {
+  data = document.getElementById('data');
+  var datatype;
+  if (typeof data == number & data > 30 & data < 200) {
+    datatype = "Gewicht";
+  } else if (typeof data == number & data > 100) {
+    datatype = "Kilokalorien";
+  } else if (typeof data == number & data < 6) {
+    datatype = "Flüssigkeitszufuhr";
+  } else {
+    datatype = "Kraft";
+  }
+  fetch('/api/succ.php', {
+    method: 'POST',
+    body: JSON.stringify({datatype: data})
+  }).then(response => response.text()).then(console.log);
+}
