91303b4b989421f278c72565bdbfa4937244787b
Alexander Baldauf <baldauf@gruener-campus-malchow.de>
2023-10-04 12:58:53 +0200

    DIE ist da

--- api/models/Values.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Values extends ProtectedModel
-{
-	protected $name = 'values';
-	protected $id = 'name';
-	protected $searchable = ['comment', 'timestamp', 'is_standard', 'type_id', 'gatherer_id'];
-	protected $insertable = ['name', 'comment', 'timestamp', 'content', 'is_standard', 'type_id', 'gatherer_id'];
-}
