ec8f51ad1eebe5a1236560506707bf49dafd4c50
MEKO <72254540+Maihoernchen@users.noreply.github.com>
2023-11-23 11:20:21 +0100

    Add files via upload

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,13 +1,8 @@
-function renderMain() {
+export function renderMain() {
     if (site=='main') {
         return(false);
     }
     var site = 'main';
     document.cookie = 'site=main';
-    document.body.topnav.innerhtml = '
-        <a href="kartoffelpc.png"><img src="kartoffelpc.png" style="width:4vw;height:4vw;"></a>
-        <a href="./login/login.php">You</a>
-        <a href="statistics.html"><span class="material-symbols-outlined">bar_chart</span></a>
-        <a href="settings.html"><span class="material-symbols-outlined">settings</span></a>
-    ';
+    document.getElementById('header').innerhtml = ('<a href="kartoffelpc.png"><img src="kartoffelpc.png" style="width:4vw;height:4vw;"></a><a href="./login/login.php">You</a><a href="statistics.html"><span class="material-symbols-outlined">bar_chart</span></a><a href="settings.html"><span class="material-symbols-outlined">settings</span></a>');
 }
