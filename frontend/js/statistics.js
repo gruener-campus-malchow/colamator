1a3eebb0691164aa95b0c7dd37b772952ca02683
Macrex1337 <105010791+Macrex1337@users.noreply.github.com>
2023-11-30 11:33:45 +0100

    Update statistics.js

--- frontend/js/statistics.js
+++ frontend/js/statistics.js
@@ -1,3 +1,4 @@
+import { renderMain } from "./main.js";
 export function renderStats() {
     if (site=='stats' && document.cookie('site')=='stats') {
         return(false);
@@ -6,9 +7,20 @@ export function renderStats() {
     document.cookie = 'site=stats';
     let head = document.getElementById('header');
     let body = document.getElementById('main');
-    // Provisorisches PP, bevor wir echte PBs vond er API bekommen
-    let profilePicture = '<img src="kartoffelpc.png" style="width:4vw;height:4vw;">';
-    // Provisorischer UN
-    let username = 'Macher Max'
-    head.innerHTML = '<button onclick="renderPP">'+profilePicture+'</button>'+username+'<button onclick="renderStats"><span class="material-symbols-outlined">bar_chart</span></button><a href="settings.html"><span class="material-symbols-outlined">settings</span></a>';
+    body.innerHTML='<!DOCTYPE html>
+<button id="return" style="font-family: Material Symbols Outlined">arrow_back</button>
+<h1>Statistiken</h1>';
+     document.getElementById('return').addEventListener("click", renderMain);
+}
+
+    fetch('/api/succ.php', {
+    method: 'GET',
+    body: JSON.stringify({datatype: data})
+  }).then(response => response.json()).then(console.log);
+ function btngen() {
+	var btn = document.createElement("BUTTON");
+	var text = document.createTextNode(datatype);
+	btn.appendChild(text);
+	var newbutton = document.getElementById("button");
+  	document.body.insertBefore(btn,newbutton);
 }
