e6b68eb9cc814f786ce16f92a8e1e2a86f715cd8
Noyavy <githubuser1@web.de>
2023-12-21 10:41:01 +0100

    updated user table creation

--- api/models/User.php
+++ api/models/User.php
@@ -15,8 +15,43 @@ class User extends Model
 		// get id from user we just created
 		$user_id = $this->db->getLastInsertId();
 		
-		// create user data table
-		$createSQL="CREATE TABLE " . $user_id . "_data (id_msg INT PRIMARY KEY AUTO_INCREMENT, timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, data BLOB, datatype_fk INT, group_fk INT, sender_fk INT, comment varchar(255))";
+		// create user values table
+		$createSQL="CREATE TABLE " . $user_id . "__values (
+					value_name VARCHAR(200) PRIMARY KEY,
+					value_comment TEXT,
+					timestamp INTEGER DEFAULT CURRENT_TIMESTAMP,
+					content BLOB,
+					is_standard INTEGER,
+					typeID VARCHAR(200) NOT NULL,
+					gathererID VARCHAR(200) NOT NULL,
+					groupID VARCHAR(200) NOT NULL,
+					CONSTRAINT `fk_user_values_to_type`
+						FOREIGN KEY (typeID) REFERENCES types(type_name)
+						ON DELETE CASCADE
+						ON UPDATE RESTRICT,
+					CONSTRAINT `fk_user_values_to_groups`
+						FOREIGN KEY (groupID) REFERENCES groups_universe(groupID)
+						ON DELETE CASCADE
+						ON UPDATE RESTRICT,
+					CONSTRAINT `fk_user_values_to_gatherer`
+						FOREIGN KEY (gathererID) REFERENCES gatherers(username)
+						ON DELETE CASCADE
+						ON UPDATE RESTRICT
+					);";
+		$this->api_response($this->db->query($createSQL, $params));
+		
+		// create user gatherer table
+		$createSQL="CREATE TABLE " . $user_id . "_gatherers (
+					username VARCHAR(200) PRIMARY KEY,
+					last_seen INTEGER DEFAULT CURRENT_TIMESTAMP
+					);";
+		$this->api_response($this->db->query($createSQL, $params));
+		
+		// create user type table
+		$createSQL="CREATE TABLE " . $user_id . "_types (
+					type_name VARCHAR(200) PRIMARY KEY,
+					label VARCHAR(200)
+					);";
 		$this->api_response($this->db->query($createSQL, $params));
 	}
 }
