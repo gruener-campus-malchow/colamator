0da89722eb08c9299af932fdbc4760a3d0331f01
Macrex1337 <105010791+Macrex1337@users.noreply.github.com>
2023-12-21 10:58:04 +0100

    Update statistics.js

--- frontend/js/statistics.js
+++ frontend/js/statistics.js
@@ -1,4 +1,6 @@
 import { saveSiteState } from "./index.js";
+import { renderMain } from './main.js';
+
 export function renderStats() {
     if (site=='stats' && document.cookie('site')=='stats') {
         return(false);
@@ -7,20 +9,19 @@ export function renderStats() {
     saveSiteState('stats');
     let head = document.getElementById('header');
     let body = document.getElementById('main');
-    body.innerHTML='<!DOCTYPE html>
-<button id="return" style="font-family: Material Symbols Outlined">arrow_back</button>
-<h1>Statistiken</h1>';
+    head.innerHTML = '';
+    body.innerHTML = '';    
+    body.innerHTML='<button id="return" style="font-family: Material Symbols Outlined">arrow_back</button><h1>Statistiken</h1>';
      document.getElementById('return').addEventListener("click", renderMain);
-}
 
     fetch('colamator/api/user/', {
     method: 'GET',
-    body: JSON.stringify({datatype: data})
+    body: JSON.stringify({dataype: data})
   }).then(response => response.json()).then((datatype)=> { array.forEach(function(datatype) {
   button = document.createElement('button');
   button.id = datatype;
   button.innerHTML = 'Your Mom '+datatype;
   console.log(button);
   document.body.appendChild(button);
-}););}
-
+});});
+}
