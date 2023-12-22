4a51406d475fc8fbf3a7cb84d9b41cd8e325de21
NoYavy <noyavy@mailbox.org>
2023-12-22 01:22:02 +0100

    implemented getSingle()

--- api/models/Tables.php
+++ api/models/Tables.php
@@ -5,22 +5,22 @@ class Tables extends Model
 	protected $name = 'tables';
 	public function getAll($filter)
 	{
-		$query="SHOW TABLES;";
-		$this->api_response($this->db->query($query, $params));
+		$query = "SHOW TABLES;";
+		$this->api_response($this->db->query($query));
 	}
-	public function getSingle($identifier) {
-		// prevent SQL injection
-		$pattern = "\b(ALTER|CREATE|DELETE|DROP|EXEC(UTE){0,1}|INSERT( +INTO){0,1}|MERGE|SELECT|UPDATE|UNION( +ALL){0,1})\b";
-		if (preg_match($pattern, $identifier)) {
-			http_response_code(403);
-			die("Be a nice person, don't inject SQL");
-		}
-		// build query
-		$query="SELECT * FROM " . $identifier . ";";
-		$result = $this->db->query($query, $params)
+	public function getSingle($identifier)
+	{
+		// // prevent SQL injection
+		$query = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ? LIMIT 1;";
 		// if table doesn't exist, return a 404
-		if (!$result) $this->api_response('Item Does Not Exist', 404);
-		// else return the item; [0] is needed since DB::query() returns the element wrapped in an array
-		else $this->api_response($result[0]);
+		if (!$this->db->query($query, [$identifier])) {
+			$this->api_response('Item Does Not Exist', 404);
+		} else {
+			// build query
+			$query = "SELECT * FROM $identifier;";
+			// return the item; [0] is needed since DB::query() returns the element wrapped in an array
+			$this->api_response($this->db->query($query));
+
+		}
 	}
 }
