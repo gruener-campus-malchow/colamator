582464b282713abaa856046e41c4619850d81c93
MEKO <72254540+Maihoernchen@users.noreply.github.com>
2024-01-22 16:59:46 +0100

    Updated SQL statements to new erm

--- doc/createTables_maria-DB.sql
+++ doc/createTables_maria-DB.sql
@@ -1,22 +1,23 @@
-CREATE TABLE types (
+CREATE TABLE typ (
 type_name VARCHAR(200) PRIMARY KEY,
 label VARCHAR(200)
 );
 
-CREATE TABLE gatherers (
+CREATE TABLE gatherer (
 username VARCHAR(200) PRIMARY KEY,
-last_seen INTEGER DEFAULT CURRENT_TIMESTAMP
+last_seen INTEGER DEFAULT CURRENT_TIMESTAMP,
+mail VARCHAR(200),
+password_hash,
 );
 
-CREATE TABLE `values` (
-value_name VARCHAR(200) PRIMARY KEY,
+CREATE TABLE 'value' (
 value_comment TEXT,
-timestamp INTEGER DEFAULT CURRENT_TIMESTAMP,
+timestamp INTEGER DEFAULT CURRENT_TIMESTAMP PRIMARY KEY,
 content BLOB,
-is_standard INTEGER,
+is_standard BOOLEAN,
 typeID VARCHAR(200) NOT NULL,
 gathererID VARCHAR(200) NOT NULL,
-groupID VARCHAR(200) NOT NULL,
+groupID CHAR(16) NOT NULL,
 CONSTRAINT `fk_user_values_to_type`
     FOREIGN KEY (typeID) REFERENCES types(type_name)
     ON DELETE CASCADE
@@ -29,6 +30,12 @@ CONSTRAINT `fk_user_values_to_gatherer`
     FOREIGN KEY (gathererID) REFERENCES gatherers(username)
     ON DELETE CASCADE
     ON UPDATE RESTRICT
+);
+
+CREATE TABLE 'group' (
+name VARCHAR(200) NOT NULL,
+description TEXT,
+pk_64 CHAR(16) PRIMARY KEY,
 );
 
 
